49b1022a917e1db59a3ed3b7c34fdb99023c7eee
FRANVIS JUGUILON <frco.juguilon.phinmaed.up@gmail.com>
2023-12-07 02:50:37 +0800

    commit

--- app/src/main/java/com/juguilon/francis/block3/p1/quiz/MainActivity2.kt
+++ app/src/main/java/com/juguilon/francis/block3/p1/quiz/MainActivity2.kt
@@ -2,10 +2,18 @@ package com.juguilon.francis.block3.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.EditText
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity2 : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
     }
 }
+
+
