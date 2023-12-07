3443eda13055e2f9566a361cde09bdd76c696de4
FRANVIS JUGUILON <frco.juguilon.phinmaed.up@gmail.com>
2023-12-08 02:24:33 +0800

    commit

---
+++ app/src/main/java/com/juguilon/francis/block3/p1/quiz/January.kt
@@ -0,0 +1,13 @@
+package com.juguilon.francis.block3.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class January : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_january)
+
+
+    }
+}
---
+++ app/src/main/java/com/juguilon/francis/block3/p1/quiz/Todo.kt
@@ -0,0 +1,6 @@
+package com.juguilon.francis.block3.p1.quiz
+
+data class Todo (
+    val title: String,
+    var isChecked: Boolean = false
+        )
